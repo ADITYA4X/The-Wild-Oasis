e6a2171a7babba2edef5a278b4fca36575502073
aditya <adityakunghadkar94.com>
2024-06-08 04:38:56 +0530

    Updated with React Hook Form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
-import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   /*
   useEffect(function () {
     getCabins().then((data) => console.log(data));
@@ -16,8 +20,14 @@ function Cabins() {
         <Heading as="h1">All cabins</Heading>
         <p>Filter / Sort</p>
       </Row>
+
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
