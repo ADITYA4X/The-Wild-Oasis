f132e37f24739dcdb7f9377e0298d98728e97c0e
aditya <adityakunghadkar94.com>
2024-06-23 18:16:57 +0530

    Final Touch

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -32,6 +32,7 @@ const StyledSalesChart = styled(DashboardBox)`
   }
 `;
 
+/*
 const fakeData = [
   { label: "Jan 09", totalSales: 480, extrasSales: 20 },
   { label: "Jan 10", totalSales: 580, extrasSales: 100 },
@@ -63,6 +64,7 @@ const fakeData = [
   { label: "Feb 05", totalSales: 1400, extrasSales: 600 },
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
+*/
 
 function SalesChart({ bookings, numDays }) {
   const { isDarkMode } = useDarkMode();
